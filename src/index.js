782afaad62ebb28a6d4de3232b77e97b689eab1e
makiko-yamada <100193093+makiko-yamada@users.noreply.github.com>
2022-03-11 22:18:39 +0900

    デフォルト値

--- src/index.js
+++ src/index.js
@@ -1,11 +1,2 @@
-const myProfile = {
-  name: "makiko",
-  age: 27
-};
-
-const message1 = `私の名前は${myProfile.name}です。`;
-console.log(message1);
-
-const { name, age } = myProfile;
-const message2 = `私の名前は${name}です。年齢は${age}だ`;
-console.log(message2);
+const sayHello = (name = "ゲスト") => console.log(`こんにちは${name}さん`);
+sayHello();
