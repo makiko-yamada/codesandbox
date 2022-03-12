c07cb7b5c6d993c3e62e1c2a74eb1cd05ca0c31c
makiko-yamada <100193093+makiko-yamada@users.noreply.github.com>
2022-03-12 09:25:14 +0900

    map

--- src/index.js
+++ src/index.js
@@ -1,2 +1,15 @@
-const sayHello = (name = "ゲスト") => console.log(`こんにちは${name}さん`);
-sayHello();
+const nameArr = ["田中", "山田", "鈴木"];
+
+//配列をループして何か処理する（従来のやり方）
+for (let index = 0; index < nameArr.length; index++) {
+  console.log(nameArr[index]);
+}
+
+//配列をループして何か処理する（map）
+nameArr.map((name) => console.log(name));
+
+//リターンされた結果に基づいて新しい配列を作る
+const nameArr2 = nameArr.map((name) => {
+  return name;
+});
+console.log(nameArr2);
